c3de057d1c37af7817766265872c11b1c63b5670
k_c_sh_7B <k_c_sh_7B@protonmail.com>
2024-10-05 19:44:37 -0400

    added the auth token.json error message

--- gmail_auth.py
+++ gmail_auth.py
@@ -9,23 +9,20 @@ SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
 def authenticate_gmail():
     credentials = None
 
-    # Check if credentials.json is provided through environment variable
+    # Check for credentials.json from environment
     creds_json = os.getenv('GOOGLE_CREDENTIALS')
     if creds_json:
         credentials_data = json.loads(creds_json)
         with open('credentials.json', 'w') as file:
             json.dump(credentials_data, file)
 
-        # Use InstalledAppFlow for device authorization (console mode)
+        # Use InstalledAppFlow with local server method for Azure
         flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
-        credentials = flow.run_console()  # Use console flow instead of local server
-
-    # If credentials exist, check if they're valid or need refresh
-    if credentials and credentials.expired and credentials.refresh_token:
-        credentials.refresh(Request())
+        credentials = flow.run_local_server(port=0)  # Use local server for OAuth
 
     else:
         raise Exception("No credentials provided")
 
     return credentials
+
 
